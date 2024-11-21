9dd54e8e168a2d4e95a9958352731a00c361b0e0
Daniel Togey <33652822+danielkosgei@users.noreply.github.com>
2024-11-21 06:19:25 +0300

    Update HomeScreen.kt

--- app/src/main/java/com/thenewkenya/ingrediet/HomeScreen.kt
+++ app/src/main/java/com/thenewkenya/ingrediet/HomeScreen.kt
@@ -21,29 +21,5 @@ import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                navController.navigate("create")
-            }) {
-                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
-            }
-        },
-        modifier = Modifier.fillMaxSize()
-    ) { innerPadding ->
-        // Apply innerPadding to the Column modifier
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding), // Use innerPadding here
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Home Screen",
-                fontSize = 20.sp
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-    }
+    
 }
